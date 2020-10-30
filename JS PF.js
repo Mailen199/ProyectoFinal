91aa8ec789872d00df6bae5c45e857f70878de62
FIRST_NAME LAST_NAME <OUR_NAME@example.com>
2020-10-30 14:13:53 -0300

    actualizacion

--- JS PF.js
+++ JS PF.js
@@ -15,3 +15,49 @@ function cerrarMapa(){
     abrir.hidden=false;
 
 }
+function ponerFoco(){
+    var sobreFoxy = document.getElementById("articuloFoxy");
+    sobreFoxy.focus();
+}
+setInterval(cambiarImagen,3000);
+var foto=1;
+function cambiarImagen(){
+    if(foto===1){
+        document.getElementById("1").hidden = true;
+        document.getElementById("2").hidden = false;
+        foto++;
+    }else if(foto===2){
+        document.getElementById("2").hidden = true;
+        document.getElementById("3").hidden = false;
+        foto++;
+    }else if(foto===3){
+        document.getElementById("3").hidden = true;
+        document.getElementById("4").hidden = false;
+        foto++;
+    }else if(foto===4){
+        document.getElementById("4").hidden = true;
+        document.getElementById("5").hidden = false;
+        foto++;
+    }else if(foto===5){
+        document.getElementById("5").hidden = true;
+        document.getElementById("6").hidden = false;
+        foto++;
+    }else if(foto===6){
+        document.getElementById("6").hidden = true;
+        document.getElementById("7").hidden = false;
+        foto++;
+    }else if(foto===7){
+        document.getElementById("7").hidden = true;
+        document.getElementById("8").hidden = false;
+        foto++;
+    }else if(foto===8){
+        document.getElementById("8").hidden = true;
+        document.getElementById("9").hidden = false;
+        foto++;
+    }else if(foto===9){
+        document.getElementById("9").hidden = true;
+        document.getElementById("1").hidden = false;
+        foto=1;
+    }
+    
+}
